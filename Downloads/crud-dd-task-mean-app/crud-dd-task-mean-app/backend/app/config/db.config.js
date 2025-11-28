1e108ca0d82020056f861f28affc06577d7a4e3d
Yaswanth Boina <boinayashwanth@gmail.com>
2025-11-28 14:57:42 +0530

    Update config for Docker and Nginx

--- Downloads/crud-dd-task-mean-app/crud-dd-task-mean-app/backend/app/config/db.config.js
+++ Downloads/crud-dd-task-mean-app/crud-dd-task-mean-app/backend/app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: "mongodb://localhost:27017/dd_db"
+  url: process.env.MONGO_URL ||"mongodb://localhost:27017/tutorials_db"
 };
--- Downloads/crud-dd-task-mean-app/crud-dd-task-mean-app/frontend/src/app/services/tutorial.service.ts
+++ Downloads/crud-dd-task-mean-app/crud-dd-task-mean-app/frontend/src/app/services/tutorial.service.ts
@@ -3,7 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://localhost:8080/api/tutorials';
+const baseUrl = '/api/tutorials';
+
 
 @Injectable({
   providedIn: 'root'
